14f22577567744dee5d5040a274f0cc087b4df97
Jude Dickson <justclassic24@gmail.com>
2021-08-31 07:12:08 +0100

    file to file_name upload

--- api/upload/index.js
+++ api/upload/index.js
@@ -3,6 +3,6 @@ const upload = require('@lib/storage');
 module.exports = async(app)=>{
     app.post('/api/upload', upload.single('file_folder'), (req, res)=>{
         console.log(req.body.filename);
-        res.json(`Sent ${req.file.originalname} successfully`);
+        res.status(201).json(`Sent ${req.file.originalname} successfully`);
     });
 }
