a1b4357f2f1012f9ae9d15e84dbcb2dc86c667e0
Jude Dickson <justclassic24@gmail.com>
2021-08-31 01:48:37 +0100

    heroku uploaded

---
+++ secrets/config/index.js
@@ -0,0 +1,9 @@
+const prod = {
+    host: "https://onestashapi.heroku.com"
+}
+
+const dev = {
+    host: "http://localhost:5000"
+}
+
+exports.server = prod;
---
+++ secrets/mail/auth/index.js
@@ -0,0 +1,9 @@
+exports.gmail = {
+    user: 'mail.onestash@gmail.com',
+    pass: 'OnePass123456'
+  }
+
+exports.ethereal = {
+  user: 'ardella.berge63@ethereal.email',
+  pass: '3NXZruHGuK9zHxh8cP'
+}
