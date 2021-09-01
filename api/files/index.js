2cc58b1f2e30d04191851ba04b562c93130f2c6b
Jude Dickson <justclassic24@gmail.com>
2021-09-01 15:31:16 +0100

    view files for frontend

--- api/files/index.js
+++ api/files/index.js
@@ -7,8 +7,39 @@ module.exports = (app)=>{
             if (err) res.status(500).json(err);
             onedriveApi.items.listChildren({
                 accessToken: response.accessToken,
-            }).then((childrens) => {
-                res.status(200).json(childrens);
+            }).then(async (childrens) => {
+                var files = [],
+                    folders = [];
+                await Promise.all(childrens.value.map(async (child)=>{
+                    if (child.file != null || child.file != undefined){
+                        files.push({
+                            file_name: child.name,
+                            id: child.id,
+                            size: child.size,
+                            type: child.file.mimeType,
+                            downloadUrl: child['@microsoft.graph.downloadUrl'],
+                            viewUrl: child.webUrl,
+                            createdDateTime: "2021-09-01T11:13:28.8Z",
+                            lastModifiedDateTime: "2021-09-01T11:13:28.8Z"
+                        });
+                        console.log(child.name);
+                    }
+                    
+                    else{
+                        folders.push({
+                            folder_name: child.name,
+                            id: child.id,
+                            size: child.size,
+                            downloadUrl: child['@microsoft.graph.downloadUrl'],
+                            viewUrl: child.webUrl,
+                            createdDateTime: "2021-09-01T11:13:28.8Z",
+                            lastModifiedDateTime: "2021-09-01T11:13:28.8Z"
+                        });
+                    }
+                    
+                }));
+                console.log(childrens)
+                res.status(200).json({number: childrens["@odata.count"], records: files});
             }).catch((err)=>{
                 res.status(403).json(`Error: ${err}`);
             });
